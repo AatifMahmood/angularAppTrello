a4853a105838999624cbcc027fa4ab54481b8f79
AatifMahmood <atifali.7220@gmail.com>
2018-03-10 22:34:25 +0500

    some styling and changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
+// Angular packages
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import {RouterModule,Routes} from "@angular/router";
+// Angular Componenets
 import { AppComponent } from './app.component';
 import { LoginComponentComponent } from './login-component/login-component.component';
 import { DashboardComponentComponent } from './dashboard-component/dashboard-component.component';
+import { AllboardsComponentComponent } from './allboards-component/allboards-component.component';
 
+// Angular Material Components
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import { AllboardsComponentComponent } from './allboards-component/allboards-component.component';
+import {MatTooltipModule} from '@angular/material/tooltip';
+
+// Angular 3rd-Party Packages
+import {ScrollbarModule} from "ngx-scrollbar";
+const appRoutes:Routes =[
+  {path:'login',component:LoginComponentComponent,pathMatch:'full'},
+  {path:'dashboard',component:DashboardComponentComponent, pathMatch:'full'},
+  {path:'',redirectTo:'/login', pathMatch:'full'},
+];
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,12 +37,15 @@ import { AllboardsComponentComponent } from './allboards-component/allboards-com
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    ScrollbarModule,
+    RouterModule.forRoot(appRoutes),
     MatFormFieldModule,
     MatInputModule,
     MatCheckboxModule,
     MatButtonModule,
     MatIconModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
